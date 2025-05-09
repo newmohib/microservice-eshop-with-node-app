f82e6444bdae611e91e6c9e1818ec8045ef08e56
newmohib <@@mohib100@@>
2025-05-09 11:26:31 +0600

    added inventory model

--- services/inventory/src/index.ts
+++ services/inventory/src/index.ts
@@ -1,37 +1,58 @@
-//express app
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
 
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import { z } from "zod";
+import {
+	createInventory,
+	getInventoryById,
+	getInventoryDetails,
+	updateInventory,
+} from './controllers';
 
-// dotenv initialization
-import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
-app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
-app.get("/health", (_req, res) => {
-  res.status(200).json({ status: "UP" });
+app.get('/health', (_req, res) => {
+	res.status(200).json({ status: 'UP' });
 });
+
+// app.use((req, res, next) => {
+// 	const allowedOrigins = ['http://localhost:8081', 'http://127.0.0.1:8081'];
+// 	const origin = req.headers.origin || '';
+
+// 	if (allowedOrigins.includes(origin)) {
+// 		res.setHeader('Access-Control-Allow-Origin', origin);
+// 		next();
+// 	} else {
+// 		res.status(403).json({ message: 'Forbidden' });
+// 	}
+// });
+
+// routes
+app.get('/inventories/:id/details', getInventoryDetails);
+app.get('/inventories/:id', getInventoryById);
+app.put('/inventories/:id', updateInventory);
+app.post('/inventories', createInventory);
 
 // 404 handler
 app.use((_req, res) => {
-  res.status(404).json({ error: "not found" });
+	res.status(404).json({ message: 'Not found' });
 });
 
-// 500 handler
+// Error handler
 app.use((err, _req, res, _next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Internel server error" });
+	console.error(err.stack);
+	res.status(500).json({ message: 'Internal server error' });
 });
 
 const port = process.env.PORT || 4002;
-const serviceName = process.env.SERVICE_NAME || "inventory-service";
+const serviceName = process.env.SERVICE_NAME || 'Inventory-Service';
+
 app.listen(port, () => {
-  console.log(`Inventory service listening on port ${port}`);
+	console.log(`${serviceName} is running on port ${port}`);
 });
